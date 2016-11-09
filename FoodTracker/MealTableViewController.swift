eb7737c92c691106cb10d5664bdae004497c1ca5
ronatory <ronatory@gmail.com>
2016-11-09 22:49:27 +0100

    Add edit and delete behaviour

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -17,6 +17,9 @@ class MealTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Use the edit button item provided by the table view controller
+    navigationItem.leftBarButtonItem = editButtonItem
+    
     // Load the sample data
     loadSampleMeals()
   }
@@ -39,6 +42,24 @@ class MealTableViewController: UITableViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  // Override to support editing the table view
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      // Delete the row from the data source
+      meals.remove(at: indexPath.row)
+      
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+      // Create a new instance of the appropriate class, insert it into the array and add a new row to the table view
+    }
+  }
+  
+  // Override to support conditional editing og the table view
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable
+    return true
   }
   
   // MARK: - Table view data source
@@ -71,12 +92,42 @@ class MealTableViewController: UITableViewController {
   
   @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
     if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
-      // Add a new meal
-      // Note: computes the location in the table view where the new table view cell representing the new meal
-      // will be inserted and stores it in a local constant called newIndexPath
-      let newIndexPath = IndexPath(row: meals.count, section: 0)
-      meals.append(meal)
-      tableView.insertRows(at: [newIndexPath], with: .bottom)
+      
+      // checks weather a row in the table view is selected. If yes, that means a user tapped one of the table views
+      // cells to edit a meal. In other words, this if statement gets executed if an existing meal is being edited
+      if let selectedIndexPath = tableView.indexPathForSelectedRow {
+        // Update an existing meal
+        meals[selectedIndexPath.row] = meal
+        tableView.reloadRows(at: [selectedIndexPath], with: .none)
+      } else {
+        // Add a new meal, user tapped on the add button
+        // Note: computes the location in the table view where the new table view cell representing the new meal
+        // will be inserted and stores it in a local constant called newIndexPath
+        let newIndexPath = IndexPath(row: meals.count, section: 0)
+        meals.append(meal)
+        tableView.insertRows(at: [newIndexPath], with: .bottom)
+      }
+    }
+  }
+  
+  // MARK: Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ShowDetail" {
+      let mealDetailViewController = segue.destination as! MealViewController
+      
+      // Get the cell that generated this segue
+      // fetches the Meal object corresponding to the selected cell in the table view
+      // it then assigns that Meal object to the meal property of the destination view controller an instance of
+      // MealViewController
+      if let selectedMealCell = sender as? MealTableViewCell {
+        let indexPath = tableView.indexPath(for: selectedMealCell)!
+        let selectedMeal = meals[indexPath.row]
+        mealDetailViewController.meal = selectedMeal
+      }
+    } else if segue.identifier == "AddItem" {
+      print("Adding new meal")
     }
   }
   
--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -29,6 +29,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     // Handle the text field's user input through delegate callbacks.
     nameTextField.delegate = self
     
+    // Set up views if editing an existing Meal
+    if let meal = meal {
+      navigationItem.title =  meal.name
+      nameTextField.text   =  meal.name
+      photoImageView.image =  meal.photo
+      ratingControl.rating =  meal.rating
+    }
+    
     // Enable the Save button only if the text field has a valid Meal name
     checkValidMealName()
   }
@@ -77,7 +85,18 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
   
   // MARK: Navigation
   @IBAction func cancel(_ sender: UIBarButtonItem) {
-    dismiss(animated: true, completion: nil)
+    
+    // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways
+    let isPresentingInAddMealMode = presentingViewController is UINavigationController
+    
+    if isPresentingInAddMealMode {
+      dismiss(animated: true, completion: nil)
+    } else {
+      // gets executed when the meal scene was pushed onto the navigation stack on top of the meal list scene
+      // the code within the else clause executes a method called popViewControllerAnimated, which pops the current view controller (meal scene)
+      // off the navigation stack of navigationController and performs an animation of the transition
+      navigationController!.popViewController(animated: true)
+    }
   }
   
   
